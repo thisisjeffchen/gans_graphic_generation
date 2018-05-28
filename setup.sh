8278b617587d92aa283c9cb0768f4e1bed4e1c5f
Jeff Chen <jc1@stanford.edu>
2018-05-28 03:50:45 +0000

    dir gen and setup stuff

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #TODO: add to this if it doesn't work for you!
-sudo apt-get install -y emacs  python-pip python-tk python3-pip
+sudo apt-get install -y emacs  python-pip python-tk python3-pip python-opencv python3-tk
 
-pip install cython nltk tensorflow-gpu matplotlib numpy requests
+pip install cython nltk tensorflow-gpu matplotlib numpy requests opencv-python pycocotools 
 pip install h5py nltk numpy scipy scikit_image tensorflow-gpu Theano
 
 
-pip3 install cython nltk  matplotlib numpy requests
+pip3 install cython nltk  matplotlib numpy requests opencv-python pycocotools
 pip3 install h5py nltk numpy scipy scikit_image  Theano tensorflow-gpu
 
 
--- tensorflow_version/segment_images.py
+++ tensorflow_version/segment_images.py
@@ -5,16 +5,24 @@ import cv2
 
 import pycocotools.coco as coco
 
-mycoco = coco.COCO('annotations/instances_train2017.json')
+MSCOCO_RAW_DIR = "Data/mscoco_raw/"
+PROCESSED_DIR = os.path.join(MSCOCO_RAW_DIR, "processed")
+
+mycoco = coco.COCO('Data/mscoco_raw/annotations/instances_train2017.json')
 
 annotations = {}
 
-with open('annotations/instances_val2017.json') as f:
+
+if not os.path.isdir(PROCESSED_DIR):
+  print ("Processed dir not found... creating ...")
+  os.mkdir(PROCESSED_DIR)
+
+with open('Data/mscoco_raw/annotations/instances_val2017.json') as f:
   data = json.load(f)
   for anno in data['annotations']:
     annotations[int(anno['image_id'])] = anno
 
-with open('annotations/instances_train2017.json') as f:
+with open('Data/mscoco_raw/annotations/instances_train2017.json') as f:
   data = json.load(f)
   for anno in data['annotations']:
     annotations[int(anno['image_id'])] = anno
@@ -23,16 +31,16 @@ print('Num annotations loaded:', len(annotations))
 
 pics_processed = 0
 pics_skipped = 0
-for filename in os.listdir('./train2017'):
+for filename in os.listdir('Data/mscoco_raw/train2017'):
   if filename.endswith('.jpg'):
-    img = cv2.imread('./train2017' + '/' + filename)
+    img = cv2.imread('Data/mscoco_raw/train2017' + '/' + filename)
     img_id = int(filename[0:-4])
     if img_id not in annotations:
       print('Missing annotation for img:', img_id)
       pics_skipped += 1
       continue
     img[mycoco.annToMask(annotations[img_id]) == 0] = [255, 255, 255]
-    cv2.imwrite('./processed' + '/' + filename, img)
+    cv2.imwrite('Data/mscoco_raw/processed' + '/' + filename, img)
     pics_processed += 1
     if pics_processed % 1000 == 0:
       print('finished: ', pics_processed)
