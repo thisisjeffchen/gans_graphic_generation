73cb07ae9079fe6c24ec931f21353e11102342f0
Jeff Chen <jc1@stanford.edu>
2018-05-30 00:56:15 +0000

    folder rename'

--- tensorflow_version/start_experiments.sh
+++ tensorflow_version/start_experiments.sh
@@ -1,9 +1,13 @@
+#tensorboard, commented out because port needs to chang
+#tensorboard --logdir=Data/Experiments  --port=2222 &
+
+
 #vm2
 #python3 prep_data.py --experiment=20180528_elephant_segmented_07_epochs_1000_gen_updates_10 --cat=elephant
 #python3 train.py  --experiment=20180528_elephant_segmented_07_epochs_1000_gen_updates_10 --epochs=1000 --gen_updates=10
 
-#python3 prep_data.py --experiment=20180528_elephant_segmented_07_epochs_1000_gen_updates_5 --cat=elephant
-#python3 train.py  --experiment=20180528_elephant_segmented_07_epochs_1000_gen_updates_5 --epochs=1000 --gen_updates=5
+python3 prep_data.py --experiment=20180528_elephant_segmented_07_epochs_1000_gen_updates_5 --cat=elephant
+python3 train.py  --experiment=20180528_elephant_segmented_07_epochs_1000_gen_updates_5 --epochs=1000 --gen_updates=5
 
 
 #vm4
@@ -15,8 +19,8 @@
 
 
 #vm6
-python3 prep_data.py --experiment=20180528_laptop_segmented_07_epochs_1000_gen_updates_10 --cat=laptop
-python3 train.py  --experiment=20180528_laptop_segmented_07_epochs_1000_gen_updates_10 --epochs=1000 --gen_updates=10
+#python3 prep_data.py --experiment=20180528_laptop_segmented_07_epochs_1000_gen_updates_10 --cat=laptop
+#python3 train.py  --experiment=20180528_laptop_segmented_07_epochs_1000_gen_updates_10 --epochs=1000 --gen_updates=10
 
-python3 prep_data.py --experiment=20180528_laptop_segmented_07_epochs_1000_gen_updates_5 --cat=laptop
-python3 train.py  --experiment=20180528_laptop_segmented_07_epochs_1000_gen_updates_5 --epochs=1000 --gen_updates=5
+#python3 prep_data.py --experiment=20180528_laptop_segmented_07_epochs_1000_gen_updates_5 --cat=laptop
+#python3 train.py  --experiment=20180528_laptop_segmented_07_epochs_1000_gen_updates_5 --epochs=1000 --gen_updates=5
