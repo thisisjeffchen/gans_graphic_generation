4ac6bae9c3022598c1d1337471b73c22bda777e1
Jeff Chen <jc1@stanford.edu>
2018-06-02 21:24:15 +0000

    start exp with 4th arg and post process

--- tensorflow_version/post_process.sh
+++ tensorflow_version/post_process.sh
@@ -3,7 +3,7 @@
 
 EXP=$1
 
-for EPOCH in 100 300 500
+for EPOCH in 100 300 500 700 1000
 do
     python3 generate_thought_vectors.py --experiment=${EXP}
     python3 generate_images.py --experiment=${EXP} --epoch=${EPOCH}
