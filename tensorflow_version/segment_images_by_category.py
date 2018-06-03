bf782206a284a5af4bb7b2e9e2e601c8fee8031f
Jeff Chen <dealmaker@hprb-rtr.stanford.edu>
2018-06-02 17:39:41 -0700

    --orig version added, gives original images

--- tensorflow_version/segment_images_by_category.py
+++ tensorflow_version/segment_images_by_category.py
@@ -13,6 +13,9 @@ def main ():
         help= 'Debug mode works on val dataset')
     parser.add_argument ('--crop', action='store_true', 
         help= 'performs cropping and centering')
+    parser.add_argument ('--orig', action='store_true', 
+        help= 'keeps original image and does not process,\
+               simply places in category folders')
 
 
     MSCOCO_RAW_DIR = "Data/mscoco_raw/"
@@ -20,11 +23,17 @@ def main ():
     args = parser.parse_args ()
     CROP = args.crop
     DEBUG = args.debug
-    
+    ORIG = args.orig
+
+    if CROP and ORIG:
+        raise Exception ("Cannot perform both cropping and keep original image")
     
     if CROP:
         PROCESSED_DIR = os.path.join(MSCOCO_RAW_DIR, "processed_segmented_cropped")
         DISREGARDED_DIR = os.path.join(MSCOCO_RAW_DIR, "disregarded_segmented_cropped")
+    elif ORIG:
+        PROCESSED_DIR = os.path.join(MSCOCO_RAW_DIR, "processed_orig")
+        DISREGARDED_DIR = os.path.join(MSCOCO_RAW_DIR, "disregarded_orig")
     else:
         PROCESSED_DIR = os.path.join(MSCOCO_RAW_DIR, "processed_segmented")
         DISREGARDED_DIR = os.path.join(MSCOCO_RAW_DIR, "disregarded_segmented")
@@ -81,41 +90,45 @@ def main ():
                 print('Missing annotation for img:', imgId)
                 pics_skipped += 1
             else:
-                # Choose the first annotation for the category only (hope it's the best one)
-                ann = coco.loadAnns(annIds)[0]
-                img[coco.annToMask(ann) == 0] = [255, 255, 255]
-    
-                num_white = np.sum(img == 255)
-                num_total = img.shape[0] * img.shape[1] * img.shape[2]
-                percent_subject = 1.0 - (num_white * 1.0 / num_total)
-    
-                if CROP:
-                    # compute crop dimensions for subject
-                    rows = np.any(img-255, axis=1)
-                    cols = np.any(img-255, axis=0)
-                    rmin, rmax = np.where(rows)[0][[0, -1]]
-                    cmin, cmax = np.where(cols)[0][[0, -1]]
-                    size = max(rmax - rmin, cmax - cmin)
-                    vertical_len = (size - (rmax-rmin))
-                    horizontal_len = (size - (cmax-cmin))
-                    vertical_pad = int (vertical_len / 2)
-                    vertical_add = vertical_len % 2
-                    horizontal_pad = int(horizontal_len / 2)
-                    horizontal_add = horizontal_len % 2
-            
-                    img = cv2.copyMakeBorder(
-                        img[rmin:rmax, cmin:cmax],
-                        vertical_pad + vertical_add, vertical_pad, 
-                        horizontal_pad + horizontal_add, horizontal_pad,
-                        cv2.BORDER_CONSTANT, value=WHITE)
-    
-                if percent_subject >= THRESHOLD:
-                     
+                if ORIG:
                     cv2.imwrite(os.path.join(class_dir_processed, filename), img)
                     pics_processed += 1
                 else:
-                    cv2.imwrite(os.path.join(class_dir_disregarded, filename), img)
-                    pics_disregarded += 1
+                    # Choose the first annotation for the category only (hope it's the best one)
+                    ann = coco.loadAnns(annIds)[0]
+                    img[coco.annToMask(ann) == 0] = [255, 255, 255]
+        
+                    num_white = np.sum(img == 255)
+                    num_total = img.shape[0] * img.shape[1] * img.shape[2]
+                    percent_subject = 1.0 - (num_white * 1.0 / num_total)
+        
+                    if CROP:
+                        # compute crop dimensions for subject
+                        rows = np.any(img-255, axis=1)
+                        cols = np.any(img-255, axis=0)
+                        rmin, rmax = np.where(rows)[0][[0, -1]]
+                        cmin, cmax = np.where(cols)[0][[0, -1]]
+                        size = max(rmax - rmin, cmax - cmin)
+                        vertical_len = (size - (rmax-rmin))
+                        horizontal_len = (size - (cmax-cmin))
+                        vertical_pad = int (vertical_len / 2)
+                        vertical_add = vertical_len % 2
+                        horizontal_pad = int(horizontal_len / 2)
+                        horizontal_add = horizontal_len % 2
+                
+                        img = cv2.copyMakeBorder(
+                            img[rmin:rmax, cmin:cmax],
+                            vertical_pad + vertical_add, vertical_pad, 
+                            horizontal_pad + horizontal_add, horizontal_pad,
+                            cv2.BORDER_CONSTANT, value=WHITE)
+        
+                    if percent_subject >= THRESHOLD:
+                         
+                        cv2.imwrite(os.path.join(class_dir_processed, filename), img)
+                        pics_processed += 1
+                    else:
+                        cv2.imwrite(os.path.join(class_dir_disregarded, filename), img)
+                        pics_disregarded += 1
     
     print('Total pictures processed:', pics_processed)
     print('Total pictures disregarded due to size:', pics_disregarded)
