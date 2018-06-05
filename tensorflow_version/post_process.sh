5b79e045960ba24216f3c9fcbe1303b5b4f36a54
Jeff Chen <jc1@stanford.edu>
2018-06-05 04:44:20 +0000

    post

--- tensorflow_version/post_process.sh
+++ tensorflow_version/post_process.sh
@@ -7,7 +7,8 @@ python3 generate_thought_vectors.py --experiment=${EXP}
 
 for EPOCH in 100 300 500 700 1000
 do
-    python3 generate_images.py --experiment=${EXP} --epoch=${EPOCH}
+    python3 generate_thought_vectors.py --experiment=${EXP}
+    python3 generate_images.py --experiment=${EXP} --epoch=${EPOCH} --vgg
     python3 copy_results.py --experiment=${EXP}
 done
 
